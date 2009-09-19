db99141b5fc16b68ef3986b300f6fe4eb2cc47ad
Danny Yoo <dyoo@cs.wpi.edu>
2009-09-19 01:46:26 -0400

    Ugh, ugly and complicated state machine.

--- android-1.5/src/plt/playlist/PlaylistPlayer.java
+++ android-1.5/src/plt/playlist/PlaylistPlayer.java
@@ -25,19 +25,32 @@ import android.util.Log;
 // finicky about running in the UI thread, so all of our operations
 // must run from the handler.
 
+//
+// uninitialized 
+//    |
+//    V
+// about to play ----> playing a song ----> delaying between songs
+//                            ^                   |
+//                            |                   |
+//                            +-------------------+
+// 
+//
+// with every state having an arrow to "STOPPED"
+
+
 public class PlaylistPlayer {
     private MediaPlayer mediaPlayer;
     private Handler handler;
     private Activity activity;
 
-
-    private int volume;
     private int currentSongIndex;
     private int delayBetweenSongs;
     private List<Uri> songs;
 
-    private boolean isCurrentlyPlaying;
-    private boolean isStopped;
+
+    private enum State { UNINITIALIZED, ABOUT_TO_PLAY, STOPPED, PLAYING_A_SONG, DELAYING_BETWEEN_SONGS };
+
+    private State currentState;
 
 
     public PlaylistPlayer(final Activity activity,
@@ -50,11 +63,9 @@ public class PlaylistPlayer {
 	this.activity = activity;
 	this.handler = handler;
 	this.handler.post(new Runnable() { public void run() { 
-	    that.volume = 100;
 	    that.songs = record.getSongUris(activity);
 	    that.currentSongIndex = 0;
-	    that.isCurrentlyPlaying = false;
-	    that.isStopped = true;
+	    that.currentState = State.UNINITIALIZED;
 	    that.delayBetweenSongs = 2000;
 	}});
 
@@ -63,84 +74,132 @@ public class PlaylistPlayer {
     // The following methods will queue up a sequence of songs to play.
 
     public void play() {
-	Log.d("PlaylistPlayer", 
-	      "play: isCurrentlyPlaying = " + isCurrentlyPlaying);
 	final PlaylistPlayer that = this;
+
 	this.handler.post(new Runnable() {
 		public void run() {
 		    try {
-			that.isStopped = false;
-
-			if (that.mediaPlayer == null) {
-			    that.mediaPlayer = new MediaPlayer();
-			    that.mediaPlayer.setLooping(false);
-			    that.mediaPlayer.setOnPreparedListener
-				(new OnPreparedListener() {
-					public void onPrepared(final MediaPlayer mp) {
-					    that.handler.post(new Runnable() {
-						    public void run() {
-							if (! that.isStopped) {
-							    that.mediaPlayer.start();
-							    that.isCurrentlyPlaying = true;
+			switch(that.currentState) {
+			case UNINITIALIZED:
+			case STOPPED:
+			    if (that.mediaPlayer == null) {
+				that.mediaPlayer = new MediaPlayer();
+				that.mediaPlayer.setLooping(false);
+				that.mediaPlayer.setOnPreparedListener
+				    (new OnPreparedListener() {
+					    public void onPrepared(final MediaPlayer mp) {
+						that.handler.post(new Runnable() {
+							public void run() {
+							    switch(that.currentState) {
+							    case UNINITIALIZED:
+								break; // This should not happen.
+							    case ABOUT_TO_PLAY:
+								that.mediaPlayer.start();
+								that.currentState = State.PLAYING_A_SONG;
+								break;
+							    case STOPPED:
+								break;
+							    case PLAYING_A_SONG:
+								break;
+							    case DELAYING_BETWEEN_SONGS:
+								break;
+							    }
 							}
-						    }
-						});
-					}
-				    });
+						    });
+					    }
+					});
 			    
-			    that.mediaPlayer.setOnCompletionListener
-				(new OnCompletionListener() {
-					public void onCompletion(final MediaPlayer mp) {
-					    that.isCurrentlyPlaying = false;
-					    that.mediaPlayer.release();
-					    that.mediaPlayer = null;
-					    that.currentSongIndex = 
-						(that.currentSongIndex + 1) %
-						that.songs.size();
+				that.mediaPlayer.setOnCompletionListener
+				    (new OnCompletionListener() {
+					    public void onCompletion(final MediaPlayer mp) {
+						switch(that.currentState) {
+						case UNINITIALIZED:
+						    break; // shouldn't happen
+						case ABOUT_TO_PLAY:
+						    break; // shouldn't happen
+						case STOPPED:
+						    break;
+						case PLAYING_A_SONG:
+						    
+						    that.mediaPlayer.release();
+						    that.mediaPlayer = null;
+						    that.currentState = State.DELAYING_BETWEEN_SONGS;
+						    that.currentSongIndex = 
+							(that.currentSongIndex + 1) %
+							that.songs.size();
+						    that.handler.postAtTime(new Runnable() {
+							    public void run() {
+								switch(that.currentState) {
+								case UNINITIALIZED:
+								    break;
+								case ABOUT_TO_PLAY:
+								    break;
+								case STOPPED:
+								    break;
+								case PLAYING_A_SONG:
+								    break;
+								case DELAYING_BETWEEN_SONGS:
+								    that.play();
+								    break;
+								}
+							    }
+							}, SystemClock.uptimeMillis() +
+							that.delayBetweenSongs);
+						    break;
+						case DELAYING_BETWEEN_SONGS:
+						    break;// shouldn't happen
+						}
 
-					    that.handler.postAtTime(new Runnable() {
-						    public void run() {
-							if (! that.isStopped) {
-							    that.play();
-							}
-						    }
-						}, SystemClock.uptimeMillis() +
-						that.delayBetweenSongs);
-					}
+					    }
 					
-				    });
+					});
 
-			    that.mediaPlayer.setDataSource
-				(that.activity,
-				 that.songs.get(that.currentSongIndex));
-			    that.mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+				that.mediaPlayer.setDataSource
+				    (that.activity,
+				     that.songs.get(that.currentSongIndex));
+				that.mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+			    }
+
+			    that.currentState = State.ABOUT_TO_PLAY;
 			    that.mediaPlayer.prepareAsync();
-			} else if (that.isCurrentlyPlaying) {
-			    return;
-			} else if (! that.isStopped) {
-			    return;
-			} else {
-			    that.mediaPlayer.start();
-			}
+			    break;
+			    
+			case ABOUT_TO_PLAY:
+			    break;
+			case PLAYING_A_SONG:
+			    break;
+			case DELAYING_BETWEEN_SONGS:
+			    break;
+			};
 		    } catch (IOException e) {
 			e.printStackTrace();
 		    }
 		}
 	    });
     }
-
+    
 
 
     public void pause() {
 	final PlaylistPlayer that = this;
 	this.handler.post(new Runnable() {
 		public void run() {
-		    that.isStopped = true;
-		    if (that.mediaPlayer == null) {
-		        return;
-		    } else if (that.isCurrentlyPlaying) {
+		    switch(that.currentState) {
+		    case UNINITIALIZED:
+			break;
+		    case ABOUT_TO_PLAY:
+			that.currentState = State.STOPPED;
+			break;
+		    case STOPPED:
+			break;
+		    case PLAYING_A_SONG:
 			that.mediaPlayer.pause();
-			that.isCurrentlyPlaying = false;
+			that.currentState = State.STOPPED;
+			break;
+		    case DELAYING_BETWEEN_SONGS:
+			that.currentState = State.STOPPED;
+			break;
 		    }
 		}
 	    });
@@ -151,13 +210,19 @@ public class PlaylistPlayer {
 	final PlaylistPlayer that = this;
 	this.handler.post(new Runnable() {
 		public void run() {
-		    that.isStopped = true;
-		    if (that.mediaPlayer == null) {
-		        return;
-		    } else if (that.isCurrentlyPlaying) {
-			that.mediaPlayer.pause();
-			that.mediaPlayer.seekTo(0);
-			that.isCurrentlyPlaying = false;
+		    switch(that.currentState) {
+		    case UNINITIALIZED:
+			break;
+		    case ABOUT_TO_PLAY:
+			that.currentState = State.STOPPED;
+		    case STOPPED:
+			break;
+		    case PLAYING_A_SONG:
+			that.mediaPlayer.stop();
+			that.mediaPlayer.release();
+			that.mediaPlayer = null;
+		    case DELAYING_BETWEEN_SONGS:
+			that.currentState = State.STOPPED;
 		    }
 		}
 	    });
@@ -165,20 +230,6 @@ public class PlaylistPlayer {
 
 
 
-    public int getVolume() {
-	return this.volume;
-    }
-
-
-    public void setVolume(final int percent) {
-	final PlaylistPlayer that = this;
-	this.handler.post(new Runnable() {
-		public void run() {
-		    that.volume = percent;
-		}
-	    });
-    }
-    
 
 
 }
